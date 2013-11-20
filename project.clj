bd58219e156b15c9f08eb9626fba1fcbc993a671
Benjamin Beckwith <bnbeckwith@gmail.com>
2013-11-20 13:16:44 -0500

    Changed dependencies in the project.clj
    * project.clj: Updated clojure version

--- project.clj
+++ project.clj
@@ -3,16 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]
-                 [com.bnbeckwith/orgmode "0.7.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.bnbeckwith/orgmode "0.7.3"]
                  [enlive "1.1.1"]
-                 [clj-rss "0.1.3"]
-                 [me.raynes/fs "1.4.4"]
+                 [clj-rss "0.1.2"]
+                 [me.raynes/fs "1.4.0"]
                  [compojure "1.1.5"]
                  [ring "1.2.0"]
+                 [com.cemerick/pomegranate "0.2.0"]
                  [prismatic/plumbing "0.1.0"]
                  [org.clojure/tools.cli "0.2.4"]]
   :main orb.core)
-
-
-; (def c (-main "-c" "/home/bnbeckwith/work/projects/bnbeckwith.com/orb.clj"))
