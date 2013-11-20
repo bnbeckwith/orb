2a75ca9b1522e31fd644521a20ab62bdca6b7766
Benjamin Beckwith <bnbeckwith@gmail.com>
2013-11-20 13:19:34 -0500

    Reworked server code

--- src/orb/serve.clj
+++ src/orb/serve.clj
@@ -5,21 +5,15 @@
         ring.middleware.file)
   (:require [clojure.string :only (replace)]))
 
-(def ^:dynamic *server* (agent nil))
-
 (defn app [root]
-  (-> handle-dump
-      (wrap-file root {:index-files? true})))
+  (wrap-file handle-dump root {:index-files? true}))
 
 (defn serve 
-  [_ root port] 
-  (run-jetty (app root) {:port port :join? false}))
+  [root port] 
+  (agent (run-jetty (app root) {:port port :join? false})))
 
-(defn start-server 
-  ([] (start-server "~/public_html/"))
-  ([root] (start-server root 80))
-  ([root port]
-     (send-off *server* serve root port)))
+(defn start-server [agt]
+     (send-off agt (fn [j] (and (.start j) j))))
 
-(defn stop-server []
-  (send-off *server* (fn [_] nil)))
+(defn stop-server [agt]
+  (send-off agt (fn [j] (and (.stop j) j))))
